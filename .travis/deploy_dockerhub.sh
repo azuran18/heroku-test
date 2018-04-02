8ee154242874a8c002b6a2839d832eb3526f2bd2
root <alexey.mikhaylov@fastdev.se>
2018-04-02 19:58:55 +0300

    Step 7_7

--- .travis/deploy_dockerhub.sh
+++ .travis/deploy_dockerhub.sh
@@ -5,8 +5,8 @@ if [ "$TRAVIS_BRANCH" = "master" ]; then
 else
     TAG="$TRAVIS_BRANCH"
 fi
-docker build -f Dockerfile -t $DOCKER_USER:$TAG .
-echo $TRAVIS_REPO_SLUG:$TAG
-docker push $TRAVIS_REPO_SLUG
+docker build -f Dockerfile -t $DOCKER_USER/heroku-test:$TAG .
+##echo $TRAVIS_REPO_SLUG:$TAG
+docker push $DOCKER_USER/heroku-test
 
 
